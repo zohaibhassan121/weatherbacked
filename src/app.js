cf94a3436fe9211865321887731d93c0d34200a8
Zohaib ul hassan <103682369+zohaibhassan121@users.noreply.github.com>
2023-04-07 15:42:45 +0500

    Update app.js
    aceess

--- src/app.js
+++ src/app.js
@@ -6,8 +6,15 @@ const express = require('express')
 const cors = require('cors')
 const app = express();
 app.use(cors());
-
-
+app.use(function (req, res, next)
+        { res.header("Access-Control-Allow-Origin", "*");
+         res.header( "Access-Control-Allow-Methods", 
+         "GET, HEAD, OPTIONS, POST, PUT, DELETE" ); 
+         res.header( "Access-Control-Allow-Headers", 
+          "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+         
+         next();
+        });
 const port = process.env.PORT || 4000
 
 
